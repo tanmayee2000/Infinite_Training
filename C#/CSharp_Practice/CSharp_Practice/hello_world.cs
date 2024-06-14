64b389ff0e7ecfca8fdddc8a777240fbf873c62a
Tanmayee Jollu <tanmayeej@infinite.com>
2024-06-14 13:19:44 +0530

    Practice Programs

---
+++ C#/CSharp_Practice/CSharp_Practice/hello_world.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice
+{
+    class hello_world
+    {
+        static void Main()
+        {
+            string message = "Hello World!!";
+
+            Console.WriteLine(message);
+            Console.Read();
+        }
+    }
+}
