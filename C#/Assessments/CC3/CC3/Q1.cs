433ab4285c82f16e7842960edf544bf6e6385d0d
Tanmayee Jollu <tanmayeej@infinite.com>
2024-07-03 13:14:59 +0530

    CC3

---
+++ C#/Assessments/CC3/CC3/Q1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC3
+{
+    class Cricket
+    {
+        public void Points_Calculation(IEnumerable<int> Scores)
+        {
+            int No_Of_Matches = 0;
+            double Sum = 0;
+
+            // Calculate sum and count
+            foreach (int score in Scores)
+            {
+                Sum += score;
+                No_Of_Matches++;
+            }
+
+            // Calculate average
+            double Average = No_Of_Matches > 0 ? Sum / No_Of_Matches : 0;
+
+            // Display results
+            Console.WriteLine($"\nSum of scores : {Sum}");
+            Console.WriteLine($"\nAverage of scores : {Average}");
+        }
+    }
+
+    class Q2
+    {
+        static void Main()
+        {
+            Console.Write("Enter number of matches played : ");
+            int Total_Matches = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("-------------------------------");
+
+            List<int> scores = new List<int>();
+
+            // Input scores from user
+            for (int i = 0; i < Total_Matches; i++)
+            {
+                Console.Write($"Enter score for match {i + 1} : ");
+                int score = Convert.ToInt32(Console.ReadLine());
+                scores.Add(score);
+            }
+
+            Console.WriteLine("-------------------------------");
+
+            Cricket cricket = new Cricket();
+            cricket.Points_Calculation(scores);
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ C#/Assessments/CC3/CC3/Q2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC3
+{
+
+    delegate Box AdditionDelegate(Box box1, Box box2);
+    class Box
+    {
+        public double Length { get; set; }
+        public double Breadth { get; set; }
+
+        // Constructor
+        public Box(double length, double breadth)
+        {
+            Length = length;
+            Breadth = breadth;
+        }
+
+        // Method to add two Box objects
+        public static Box Add_Boxes(Box box1, Box box2)
+        {
+            double Lengths_sum = box1.Length + box2.Length;
+            double Breadths_sum = box1.Breadth + box2.Breadth;
+
+            return new Box(Lengths_sum, Breadths_sum);
+        }
+
+        public void DisplayBox3()
+        {
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine($"Box 3 Dimensions :- \nLength : {Length}  \nBreadth : {Breadth}");
+            Console.Read();
+        }
+    }
+
+    // Test class
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter dimensions for Box 1 : ");
+            Console.Write("Length : ");
+            double length1 = double.Parse(Console.ReadLine());
+            Console.Write("Breadth : ");
+            double breadth1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nEnter dimensions for Box 2 : ");
+            Console.Write("Length : ");
+            double length2 = double.Parse(Console.ReadLine());
+            Console.Write("Breadth : ");
+            double breadth2 = double.Parse(Console.ReadLine());
+
+            // Box 1 & 2 Objects
+            Box box1 = new Box(length1, breadth1);
+            Box box2 = new Box(length2, breadth2);
+
+            AdditionDelegate addDelegate = new AdditionDelegate(Box.Add_Boxes);
+            Box box3 = addDelegate(box1, box2);
+
+            box3.DisplayBox3();
+        }
+    }
+
+}
