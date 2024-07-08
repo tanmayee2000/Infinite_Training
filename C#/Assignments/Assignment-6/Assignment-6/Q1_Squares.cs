ca4c28bb95af9e1732fb3dc4d7c46986338e9d0b
Tanmayee Jollu <tanmayeej@infinite.com>
2024-07-08 23:54:55 +0530

    Assignment-6

---
+++ C#/Assignments/Assignment-6/Assignment-6/Q1_Squares.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6
+{
+    class Q1_Squares
+    {
+        static void Main()
+        {
+            // Input numbers from the user
+            Console.WriteLine("Enter numbers separated by commas : ");
+            int[] numbers = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("\nNumbers with their quares greater than 20 are : ");
+            Console.WriteLine("\n");
+
+
+            List<string> results = GetNumbers(numbers);
+
+            foreach (var res in results)
+            {
+                Console.WriteLine(res);
+            }
+            Console.Read();
+        }
+
+        static List<string> GetNumbers(int[] numbers)
+        {
+            List<string> results = (
+                from num in numbers
+                let square = num * num
+                where square > 20
+                select $"{num} -> {square}"
+            ).ToList();
+
+            return results;
+        }
+    }
+}
---
+++ C#/Assignments/Assignment-6/Assignment-6/Q2_StartingEndingLetters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6
+{
+    class Q2_StartingEndingLetters
+    {
+        static void Main()
+        {
+            // Taking input from the user
+            Console.WriteLine("Enter a list of words separated by spaces : ");
+            string input = Console.ReadLine();
+
+            // Splitting the input into an array of words
+            string[] words = input.Split(' ');
+
+            //LINQ query 
+            var result = from word in words
+                         where word.ToLower().StartsWith("a") && word.ToLower().EndsWith("m")
+                         select word;
+
+            // Display
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("\nWords starting with 'a' and ending with 'm':");
+            foreach (var word in result)
+            {
+                Console.WriteLine("-> " + word);
+            }
+            Console.Read();
+        }
+    }
+}
